e3c941831a27ac8a64565a263b2ee11c3a8f4f77
jgmuro <jgmuro@amazon.com>
2024-04-18 13:29:25 -0400

    First Change Test

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
             Data <span>-&gt;</span>
           </h2>
           <p>
-            Fully-typed real-time API with NoSQL database.
+            Fully-typed real-time API with NoSQL database. This is cool!
           </p>
         </a>
       </div>
